c9bff583b16de66de5f0c29673474b0fecc6a55d
CalvinCollison <caco4561@colorado.edu>
2022-10-16 20:25:49 -0600

    Update assignment8.js

--- assignments/assignment8/assignment8.js
+++ assignments/assignment8/assignment8.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they contemplated life for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const insertX = ["Moblin the Goblin", "Alec Bohm", "Daniel Ricciardo"];
+const insertY = ["Citizens Bank Park", "Suzuka", "the Kobold cave"];
+const insertZ = ["did a shoey", "were swarmed by fans", "did an epic air guitar solo"];
 
 randomize.addEventListener('click', result);
 
