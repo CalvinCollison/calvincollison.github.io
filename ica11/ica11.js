93c8d9daf3a5e70f5181853bf688a1a7905777fd
CalvinCollison <caco4561@colorado.edu>
2022-11-02 21:50:33 -0600

    Added ica11

---
+++ ica11/ica11.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".new-quote-button");
+button.addEventListener("click", getQuote);
+const quoteApi = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+async function getQuote(){
+    console.log("Button click");
+    let response = await fetch(quoteApi);
+    let text = await response.text();
+    let json = JSON.parse(text);
+
+    console.log(json['message']);
+    displayQuote(json['message']);
+}
+
+function displayQuote(text){
+    document.getElementById("js-quote-text").textContent = text;
+}
